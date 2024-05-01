95ba57490701c890b88c9cb36b5807541e69f57d
anisamlia <anisamlia5581@gmail.com>
2024-05-01 18:22:28 +0700

    update form

--- frontend/src/Page/scenes/form/index.tsx
+++ frontend/src/Page/scenes/form/index.tsx
@@ -1,16 +1,16 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import { Formik, FormikHelpers } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../../components/Header";
 
 interface FormValues {
-  firstName: string;
-  lastName: string;
-  email: string;
-  contact: string;
-  address1: string;
-  address2: string;
+  jobname: string;
+  jobtype: string;
+  joblevel: string;
+  jobsalary: string;
+  joblocation: string;
+  // address2: string;
 }
 
 const Form = () => {
@@ -41,6 +41,7 @@ const Form = () => {
           handleChange,
           handleSubmit,
         }) => (
+          
           <form onSubmit={handleSubmit}>
             <Box
               display="grid"
@@ -54,84 +55,85 @@ const Form = () => {
                 fullWidth
                 variant="filled"
                 type="text"
-                label="First Name"
+                label="Job Name"
                 onBlur={handleBlur}
                 onChange={handleChange}
-                value={values.firstName}
-                name="firstName"
-                error={!!touched.firstName && !!errors.firstName}
-                helperText={touched.firstName && errors.firstName}
-                sx={{ gridColumn: "span 2" }}
+                value={values.jobname}
+                name="jobname"
+                error={!!touched.jobname && !!errors.jobname}
+                helperText={touched.jobname && errors.jobname}
+                sx={{ gridColumn: "span 4" }}
               />
+              <FormControl fullWidth variant="filled" sx={{ gridColumn: "span 2" }}>
+                <InputLabel id="job-type-label">Job Type</InputLabel>
+                <Select
+                  labelId="job-type-label"
+                  id="jobtype"
+                  name="jobtype"
+                  value={values.jobtype}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={!!touched.jobtype && !!errors.jobtype}
+                >
+                  <MenuItem value="Full-time">Full-time</MenuItem>
+                  <MenuItem value="Part-time">Part-time</MenuItem>
+                  <MenuItem value="Remote">Remote</MenuItem>
+                  <MenuItem value="Internship">Internship</MenuItem>
+                </Select>
+                {touched.jobtype && errors.jobtype && (
+                  <div style={{ color: 'red' }}>{errors.jobtype}</div>
+                )}
+              </FormControl>
+              <FormControl fullWidth variant="filled" sx={{ gridColumn: "span 2" }}>
+                <InputLabel id="job-level-label">Job Level</InputLabel>
+                <Select
+                  labelId="job-level-label"
+                  id="joblevel"
+                  name="joblevel"
+                  value={values.joblevel}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={!!touched.joblevel && !!errors.joblevel}
+                >
+                  <MenuItem value="Entry Level">Entry Level</MenuItem>
+                  <MenuItem value="Mid Level">Mid Level</MenuItem>
+                  <MenuItem value="Senior Level">Senior Level</MenuItem>
+                  <MenuItem value="Lead Level">Lead Level</MenuItem>
+                </Select>
+                {touched.joblevel && errors.joblevel && (
+                  <div style={{ color: 'red' }}>{errors.joblevel}</div>
+                )}
+              </FormControl>
               <TextField
                 fullWidth
                 variant="filled"
                 type="text"
-                label="Last Name"
+                label="Salary"
                 onBlur={handleBlur}
                 onChange={handleChange}
-                value={values.lastName}
-                name="lastName"
-                error={!!touched.lastName && !!errors.lastName}
-                helperText={touched.lastName && errors.lastName}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Email"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                name="email"
-                error={!!touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
+                value={values.jobsalary}
+                name="jobsalary"
+                error={!!touched.jobsalary && !!errors.jobsalary}
+                helperText={touched.jobsalary && errors.jobsalary}
                 sx={{ gridColumn: "span 4" }}
               />
               <TextField
                 fullWidth
                 variant="filled"
                 type="text"
-                label="Contact Number"
+                label="Job Location"
                 onBlur={handleBlur}
                 onChange={handleChange}
-                value={values.contact}
-                name="contact"
-                error={!!touched.contact && !!errors.contact}
-                helperText={touched.contact && errors.contact}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Address 1"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.address1}
-                name="address1"
-                error={!!touched.address1 && !!errors.address1}
-                helperText={touched.address1 && errors.address1}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Address 2"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.address2}
-                name="address2"
-                error={!!touched.address2 && !!errors.address2}
-                helperText={touched.address2 && errors.address2}
+                value={values.joblocation}
+                name="joblocation"
+                error={!!touched.joblocation && !!errors.joblocation}
+                helperText={touched.joblocation && errors.joblocation}
                 sx={{ gridColumn: "span 4" }}
               />
             </Box>
             <Box display="flex" justifyContent="end" mt="20px">
               <Button type="submit" color="secondary" variant="contained">
-                Create New User
+                Create Job Listing
               </Button>
             </Box>
           </form>
@@ -141,28 +143,28 @@ const Form = () => {
   );
 };
 
-const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+// const phoneRegExp =
+//   /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
 
 const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  contact: yup
+  jobname: yup.string().required("required"),
+  jobtype: yup.string().required("required"),
+  joblevel: yup.string().required("required"),
+  jobsalary: yup
     .string()
-    .matches(phoneRegExp, "Phone number is not valid")
+    // .matches(phoneRegExp, "Phone number is not valid")
     .required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
+  joblocation: yup.string().required("required"),
+  // address2: yup.string().required("required"),
 });
 
 const initialValues: FormValues = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  contact: "",
-  address1: "",
-  address2: "",
+  jobname: "",
+  jobtype: "",
+  joblevel: "",
+  jobsalary: "",
+  joblocation: "",
+  // address2: "",
 };
 
 export default Form;
