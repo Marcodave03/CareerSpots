ea8420e31d9ba144a59ef12a059e886748129fa7
Marco Dave <marcodave03@gmail.com>
2024-05-07 13:35:26 +0700

    Controller done
    - User
    - Staff
    - Job
    - Company

--- backend/models/StaffModel.js
+++ backend/models/StaffModel.js
@@ -11,23 +11,5 @@ const Staff = {
     }
 };
 
-// Staff.BelongsTo(User, {
-//     foreignKey: "user-id", 
-//     onDelete: "CASCADE", 
-//     onUpdate: "RESTRICT"
-// });
-//Staff.belongsTo(Users, { onDelete: 'CASCADE', onUpdate: 'CASCADE' }); // Delete staff if user is deleted
-// Staff.belongsTo(Company, { onDelete: 'CASCADE', onUpdate: 'CASCADE' }); // Delete staff if company is deleted
-// Staff.hasMany(Job, { onDelete: 'CASCADE', onUpdate: 'CASCADE' }); 
-// Staff.belongsTo(Company, {
-//     foreignKey: "company_id", 
-//     onDelete: "RESTRICT", 
-//     onUpdate: "RESTRICT"
-// })
-// Staff.hasMany(Job, {
-//     foreignKey: "staff_id", 
-//     onDelete: "RESTRICT", 
-//     onUpdate: "RESTRICT"
-// })
 export default Staff;
 
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,12 +1,4 @@
 import express from "express";
-// import {getUsers,
-//         getUserById,
-//         createUser,
-//         updateUser,
-//         deleteUser,
-//         createPhoto,
-//         getUserss
-//     } from "../controllers/UserController.js";
 import {
     getUser,
     getUserById,
@@ -14,25 +6,48 @@ import {
     updateUser,
     deleteUser
 } from "../controllers/UsersController.js";
-//import loginController from '../controllers/LoginController.js';
+
+import {
+    getCompany,
+    createCompany,
+    deleteCompany,
+    getCompanyById
+} from "../controllers/CompanyController.js";
+
+import {
+    getStaff,
+    createStaff,
+    deleteStaff
+} from "../controllers/StaffController.js";
+
+import {
+    getJob,
+    createJob
+} from "../controllers/JobController.js";
 
 const router = express.Router(); //Function express
 
-// controller
-// router.get('/user',getUsers); // ambil semua user
-// router.get('/user/:id',getUserById); // ambil id
-//router.post('/user',createUser); 
-// router.post('/user',createPhoto);
-// router.patch('/user/:id',updateUser); 
-// router.delete('/user/:id',deleteUser); 
-// router.post('/login', loginController.loginUser);
-
+//user
 router.get('/users',getUser);
 router.get('/users/:user_id', getUserById)
 router.post('/users',createProfile);
 router.patch('/users/:user_id',updateUser);
 router.delete('/users/:user_id', deleteUser);
 
+//company
+router.get('/companies',getCompany);
+router.get('/companies/:company_id',getCompanyById);
+router.post('/companies',createCompany);
+router.delete('/companies/:company_id',deleteCompany);
+
+//staff
+router.get('/staffs',getStaff);
+router.post('/staffs',createStaff);
+router.delete('/staffs/:staff_id',deleteStaff);
+
+//job
+router.get('/job',getJob);
+router.post('/job',createJob);
 
 export default router;
 
